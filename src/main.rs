2212a6efeda9d13e43fb4045a750ad7df2657b2f
lordScrubling <lord.scrubling@gmail.com>
2022-06-03 15:27:32 -0500

    universal

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::process::Command;
+use std::fs;
 
 // maybe one day i'll figure out how to hook it to roblox updating
-fn main() {
-	let mut ls = Command::new("C:\\msys64\\usr\\bin\\sh.exe")
-		.args(["-c", "ls C:/Users/Leo/AppData/Local/Roblox/Versions/* -dt"])
-		.output()
-		.unwrap();
+fn main() -> std::io::Result<()>{
+	let mut dirs = fs::read_dir("C:\\Users\\Leo\\AppData\\Local\\Roblox\\Versions")?
+		.flatten()
+		.filter(|dir| dir.file_type().unwrap().is_dir());
 
-	let mut i = 0;
-	while i < ls.stdout.len() && ls.stdout[i] != b'\n' {
-		i += 1;
+	let mut last = dirs.next().expect("no dirs");
+	for dir in dirs {
+		if dir.metadata()?.modified()? > last.metadata()?.modified()? {
+			last = dir;
+		}
 	}
-	ls.stdout.resize(i, 0);
 
-	Command::new("cp")
-		.args(["C:\\Users\\Leo\\Documents\\ClientSettings", unsafe { std::str::from_utf8_unchecked(&ls.stdout) }, "-r"])
-		.status()
-		.unwrap();
+	let mut path = last.path().clone();
+	path.push("ClientSettings");
+	fs::create_dir(&path)?;
+	path.push("ClientAppSettings.json");
+	fs::copy("C:\\Users\\Leo\\Documents\\ClientAppSettings.json", path)?;
+
+	Ok(())
 }
